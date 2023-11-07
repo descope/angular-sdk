c61aba05a4f1676ead590e2fae020e872794595f
Piotr Bochenek <piotr@velocit.dev>
2023-11-07 21:40:57 +0100

    add token

--- projects/angular-sdk/src/lib/services/descope.interceptor.ts
+++ projects/angular-sdk/src/lib/services/descope.interceptor.ts
@@ -25,20 +25,15 @@ export class DescopeInterceptor implements HttpInterceptor {
 		next: HttpHandler
 	): Observable<HttpEvent<unknown>> {
 		if (this.shouldIntercept(request)) {
-			return next.handle(request).pipe(
+			const token = this.authService.getSessionToken();
+			if (token === null) {
+				return this.refreshAndRetry(request, next);
+			}
+			const requestWithToken = this.addTokenToRequest(request, token);
+			return next.handle(requestWithToken).pipe(
 				catchError((error: HttpErrorResponse) => {
 					if (error.status === 401 || error.status === 403) {
-						return this.authService.refreshSession().pipe(
-							switchMap((refreshed) => {
-								if (refreshed) {
-									// If session refreshed, retry the initial request
-									return next.handle(request);
-								} else {
-									// If session refresh fails, propagate the initial error
-									return throwError(() => error);
-								}
-							})
-						);
+						return this.refreshAndRetry(request, next, error);
 					} else {
 						return throwError(() => error);
 					}
@@ -49,7 +44,41 @@ export class DescopeInterceptor implements HttpInterceptor {
 		}
 	}
 
+	private refreshAndRetry(
+		request: HttpRequest<unknown>,
+		next: HttpHandler,
+		error?: HttpErrorResponse
+	) {
+		return this.authService.refreshSession().pipe(
+			switchMap((refreshed) => {
+				if (refreshed.ok && refreshed.data) {
+					const requestWithRefreshedToken = this.addTokenToRequest(
+						request,
+						refreshed.data?.sessionJwt
+					);
+					return next.handle(requestWithRefreshedToken);
+				} else {
+					return throwError(() => error ?? new Error('Could not refresh session!'));
+				}
+			})
+		);
+	}
+
 	private shouldIntercept(request: HttpRequest<unknown>): boolean {
-		return this.pathsToIntercept.length === 0 || this.pathsToIntercept.some((path) => request.url.includes(path));
+		return (
+			this.pathsToIntercept.length === 0 ||
+			this.pathsToIntercept.some((path) => request.url.includes(path))
+		);
+	}
+
+	private addTokenToRequest(
+		request: HttpRequest<unknown>,
+		token: string
+	): HttpRequest<unknown> {
+		return request.clone({
+			setHeaders: {
+				Authorization: `Bearer ${token}`
+			}
+		});
 	}
 }
