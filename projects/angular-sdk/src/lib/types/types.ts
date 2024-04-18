6f497eb0ad1a926f6e98b848bf065822d879664d
Asaf Shen <asaf@descope.com>
2024-04-18 16:30:01 +0300

    store last auth user (#119)

--- projects/angular-sdk/src/lib/types/types.ts
+++ projects/angular-sdk/src/lib/types/types.ts
@@ -3,7 +3,11 @@ import { ILogger } from '@descope/web-component';
 export class DescopeAuthConfig {
 	projectId = '';
 	baseUrl?: string;
+	// If true, tokens will be stored on local storage
+	persistTokens?: boolean;
 	sessionTokenViaCookie?: boolean;
+	// If true, last authenticated user will be stored on local storage and can accessed with getUser function
+	storeLastAuthenticatedUser?: boolean;
 	pathsToIntercept?: string[];
 }
 
