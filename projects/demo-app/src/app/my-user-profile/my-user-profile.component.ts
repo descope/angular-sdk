b34e739c16b568c653f61146dda75452ae94187b
Nir Gur Arie <nir@descope.com>
2024-04-29 22:54:32 +0300

    CR fixes

--- projects/demo-app/src/app/my-user-profile/my-user-profile.component.ts
+++ projects/demo-app/src/app/my-user-profile/my-user-profile.component.ts
@@ -10,5 +10,10 @@ export class MyUserProfileComponent {
 	theme = (environment.descopeTheme as 'light' | 'dark' | 'os') ?? 'os';
 	debugMode = environment.descopeDebugMode ?? false;
 
-	constructor(private _: Router) {}
+	onLogout(e: CustomEvent) {
+		console.log('SUCCESSFULLY LOGGED IN FROM WEB COMPONENT', e.detail);
+		this.router.navigate(['/login']).catch((err) => console.error(err));
+	}
+
+	constructor(private router: Router) {}
 }
